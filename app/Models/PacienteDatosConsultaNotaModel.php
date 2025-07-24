e8661e3a296e6206da0a616e10540a125ad3b380
DesarrolloAGZ <desarrollo.ti@agrizar.com>
2025-07-24 12:12:03 -0600

    update-prod: Ajuste en modelo de notas para mostrar en el formato correcto la fecha y horas

--- app/Models/PacienteDatosConsultaNotaModel.php
+++ app/Models/PacienteDatosConsultaNotaModel.php
@@ -8,12 +8,25 @@ use Illuminate\Support\Facades\DB;
 class PacienteDatosConsultaNotaModel extends Model
 {
   use HasFactory;
+
   protected $connection = 'pgsql';
-  protected $fillable = [
-      'id',
-  ];
+  protected $fillable = ['id'];
   protected $hidden = ['updated_at', 'borrado'];
   protected $table = 'paciente_datos_consulta_nota';
   public $timestamps = true;
 
+  protected function asDateTime($value)
+  {
+    return parent::asDateTime($value)->setTimezone(config('app.timezone'));
+  }
+
+  public function getDateFormat()
+  {
+    return 'Y-m-d H:i:s.u';
+  }
+
+  protected $casts = [
+      'created_at' => 'datetime:Y-m-d H:i:s',
+      'updated_at' => 'datetime:Y-m-d H:i:s',
+  ];
 }
